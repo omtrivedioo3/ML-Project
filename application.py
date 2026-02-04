8f5704559ac45d1fc56ba234dee7e6a546a31f9c
Om Trivedi <omtrivedioo3@gmail.com>
2026-02-04 15:27:48 +0530

    Deployement Configuration changes

--- application.py
+++ application.py
@@ -1,15 +1,14 @@
 from flask import Flask,request,render_template
 import numpy as np
 import pandas as pd
+import sys, os
 
-from sklearn.preprocessing import StandardScaler
-from src.pipeline.predict_pipeline import CustomData,PredictPipeline
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "src")))
+
+from pipeline.predict_pipeline import CustomData,PredictPipeline
 
 application=Flask(__name__)
-
 app=application
-
-## Route for a home page
 
 @app.route('/')
 def index():
@@ -26,22 +25,14 @@ def predict_datapoint():
             parental_level_of_education=request.form.get('parental_level_of_education'),
             lunch=request.form.get('lunch'),
             test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('writing_score')),
-            writing_score=float(request.form.get('reading_score'))
-
+            reading_score=float(request.form.get('reading_score')),
+            writing_score=float(request.form.get('writing_score'))
         )
-        pred_df=data.get_data_as_data_frame()
-        print(pred_df)
-        print("Before Prediction")
 
+        pred_df=data.get_data_as_data_frame()
         predict_pipeline=PredictPipeline()
-        print("Mid Prediction")
         results=predict_pipeline.predict(pred_df)
-        print("after Prediction")
         return render_template('home.html',results=results[0])
-    
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0")        
-
-
+    app.run(host="0.0.0.0")
